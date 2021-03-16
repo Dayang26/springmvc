b51c3540f9a594c0089e11f690f11c5e0cf42e56
Dayang26 <hsyy26@qq.com>
2021-03-16 19:31:47 +0800

    123

---
+++ springmvc-01-servlet/src/main/java/com/snow/servlet/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.snow.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Snow
+ * @create 2021-03-16 8:35
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
